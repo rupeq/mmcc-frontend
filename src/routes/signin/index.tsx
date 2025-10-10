84111bb54880992a6545aa5ba9672c0df37a9e3b
Artem Dereviago <multimouse123@gmail.com>
2025-10-10 14:45:54 +0300

    feat: add auth check

--- src/routes/signin/index.tsx
+++ src/routes/signin/index.tsx
@@ -1,10 +1,16 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 import { SignIn, signInSearchSchema } from "@/features/authentication";
+import { CookieUtils } from "@/lib/api/cookieUtils";
 
 const RouteComponent = () => <SignIn />;
 
 export const Route = createFileRoute("/signin/")({
   component: RouteComponent,
   validateSearch: signInSearchSchema,
+  beforeLoad: () => {
+    if (CookieUtils.isAuthenticated()) {
+      throw redirect({ to: "/" });
+    }
+  },
 });
--- src/routes/signup/index.tsx
+++ src/routes/signup/index.tsx
@@ -1,9 +1,15 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 import { SignUp } from "@/features/authentication";
+import { CookieUtils } from "@/lib/api/cookieUtils";
 
 const RouteComponent = () => <SignUp />;
 
 export const Route = createFileRoute("/signup/")({
   component: RouteComponent,
+  beforeLoad: () => {
+    if (CookieUtils.isAuthenticated()) {
+      throw redirect({ to: "/" });
+    }
+  },
 });
