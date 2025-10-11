aead12b4d786649744f3de5ca86fe39974cafa6b
Artem Dereviago <multimouse123@gmail.com>
2025-10-11 21:54:46 +0300

    feat: head management

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -1,12 +1,12 @@
 import i18next from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import HttpApi, { type HttpBackendOptions } from "i18next-http-backend";
+import Backend, { type HttpBackendOptions } from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
 import { i18nConfiguration } from "@/lib/i18n/configuration";
 
 i18next
-  .use(HttpApi)
+  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init<HttpBackendOptions>(i18nConfiguration);
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,13 +2,14 @@ import { TanStackDevtools } from "@tanstack/react-devtools";
 import { pacerDevtoolsPlugin } from "@tanstack/react-pacer-devtools";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools";
-import { createRootRoute, Outlet } from "@tanstack/react-router";
+import { createRootRoute, HeadContent, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
 import { queryClient } from "@/lib/tanstack";
 
 const RootLayout = () => (
   <QueryClientProvider client={queryClient}>
+    <HeadContent />
     <Outlet />
     <TanStackDevtools
       eventBusConfig={{
